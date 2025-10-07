6d9b41a5ce93ee517355b7c2caa31a26bbd3937f
Spider47 <simobahloul06@gmail.com>
2025-10-07 12:24:30 +0200

    init-conf-host

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "gopark-5aaa3", appId: "1:95612175459:web:666d6b87a31e3bfaae6456", storageBucket: "gopark-5aaa3.firebasestorage.app", apiKey: "AIzaSyBL-dfD1K1BvgY-qpmM_uXtbTl_vPmyLn8", authDomain: "gopark-5aaa3.firebaseapp.com", messagingSenderId: "95612175459" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
